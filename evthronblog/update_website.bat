b765460f8cd4c874eedd2e3f9433be9f7f1fe9be
Evthron <jackylam0717@gmail.com>
2024-07-18 13:55:53 +0800

    fix bug on linux

--- evthronblog/update_website.bat
+++ evthronblog/update_website.bat
@@ -1,12 +1,11 @@
 git status
 set /p MESSAGE="Commit Message: "
 hugo
-git add .
+cd D:\Blog\blog\evthronblog\public
 git add .
 git commit -m "%MESSAGE%"
 git push
-cd D:\Blog\blog\evthronblog\public
-git add .
+cd D:\Blog\blog\evthronblog
 git add .
 git commit -m "%MESSAGE%"
 git push
